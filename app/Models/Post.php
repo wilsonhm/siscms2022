d45a6637d11859e0968293b4ff3930e99021542e
wilsonhm <hwilsonadan@gmail.com>
2022-04-20 10:44:42 -0500

    first commit

---
+++ app/Models/Post.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Post extends Model
+{
+    use HasFactory;
+    public function category(){
+        return $this->belongsTo(Category::class);
+    }
+    public function user(){
+        return $this->BelongsTo(User::class);
+    }
+    public function tags(){
+        return $this->belongsToMany(Tag::class);
+    }
+    public function image(){
+        return $this->morphOne(Image::class,'imageable');
+    }
+}
